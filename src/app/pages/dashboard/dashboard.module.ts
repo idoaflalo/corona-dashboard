38900c29531a0fb2c43ef947e7b45b1ec6d1385a
Ido Aflalo <ido.aflalo@gmail.com>
2020-04-03 21:07:44 +0300

    add first chart

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { NbCardModule } from '@nebular/theme';
+import { NgModule } from "@angular/core";
+import { NbCardModule } from "@nebular/theme";
 
-import { ThemeModule } from '../../@theme/theme.module';
-import { DashboardComponent } from './dashboard.component';
+import { ThemeModule } from "../../@theme/theme.module";
+import { DashboardComponent } from "./dashboard.component";
+import { NgxEchartsModule } from "ngx-echarts";
+import { EpidemiologicGraphComponent } from "./epidemiologic-graph/epidemiologic-graph.component";
 
 @NgModule({
-  imports: [
-    NbCardModule,
-    ThemeModule,
-  ],
-  declarations: [
-    DashboardComponent,
-  ],
+  imports: [NbCardModule, ThemeModule, NgxEchartsModule],
+  declarations: [DashboardComponent, EpidemiologicGraphComponent]
 })
-export class DashboardModule { }
+export class DashboardModule {}
