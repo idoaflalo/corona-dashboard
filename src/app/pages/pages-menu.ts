9d8aad6591eb23dbc244d917f5316d5e79a1e51b
Ido Aflalo <ido.aflalo@gmail.com>
2020-04-03 23:18:37 +0300

    change font

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -3,31 +3,8 @@ import { NbMenuItem } from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'מצב כללי',
-    icon: 'home-outline',
+    icon: 'grid-outline',
     link: '/pages/dashboard',
     home: true,
-  },
-  {
-    title: 'Auth',
-    icon: 'lock-outline',
-    hidden: true,
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
   },
 ];
