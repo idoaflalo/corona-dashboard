22ae607856333ea0afabcad64222ebb1a26e5e0f
Guy Baron <guyb2000@gmail.com>
2020-04-04 17:04:37 +0300

    basic table with english column names

--- src/app/pages/dashboard/contagion-areas/contagion-areas.component.ts
+++ src/app/pages/dashboard/contagion-areas/contagion-areas.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface DataRow {
+  location: string;
+  sickCitizensCount: number;
+  percentGrowth: number;
+  percentOfPopulation: number;
+  isolatedCitizensCount: number;
+}
+
 @Component({
   selector: 'ngx-contagion-areas',
   templateUrl: './contagion-areas.component.html',
   styleUrls: ['./contagion-areas.component.css']
 })
 export class ContagionAreasComponent implements OnInit {
-
+  columns: ['location', 'sickCitizensCount', 'percentGrowth', 'percentOfPopulation', 'isolatedCitizensCount'];
+  data: DataRow[] = [
+    {
+      location: 'הרצליה',
+      sickCitizensCount: 1000,
+      percentGrowth: 5,
+      percentOfPopulation: 50,
+      isolatedCitizensCount: 500,
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
