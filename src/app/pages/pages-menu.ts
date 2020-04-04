8f2963149580403032ccc8b0d7014c729bb0c850
Ido Aflalo <ido.aflalo@gmail.com>
2020-04-04 15:19:37 +0300

    change menu

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,8 +2,8 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'מצב כללי',
-    icon: 'grid-outline',
+    title: 'נדבקים וחולים',
+    icon: 'activity-outline',
     link: '/pages/dashboard',
     home: true,
   },
