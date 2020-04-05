bb3738dabc85935125b8002042eb997eff64bc84
itamar244 <itamary24@gmail.com>
2020-04-05 18:42:17 +0300

    add vertical bar legend, move data to component

--- src/app/pages/dashboard/infected-by-gender/infected-by-gender.component.ts
+++ src/app/pages/dashboard/infected-by-gender/infected-by-gender.component.ts
@@ -1,4 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { VerticalBarData } from '../vertical-bar/vertical-bar.component';
+
+const ages = [0, 18, 30, 42, 55, 67];
+const ageSections = ages.map((step, i, arr) =>
+  i === arr.length - 1
+    ? `+${step}`
+    : `${step + (i === 0 ? 0 : 1)}-${ages[i + 1]}`,
+);
 
 @Component({
   selector: 'ngx-infected-by-gender',
@@ -6,4 +14,18 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./infected-by-gender.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InfectedByGenderComponent {}
+export class InfectedByGenderComponent {
+  public readonly data = [
+    {male: 800, female: 50},
+    {male: 10000, female: 600},
+    {male: 3000, female: 1000},
+    {male: 9500, female: 800},
+    {male: 9900, female: 1100},
+    {male: 100, female: 1200},
+  ];
+  public readonly verticalTabData: VerticalBarData[] = ageSections.map((section, i) => ({
+    label: section,
+    left: this.data[i].female,
+    right: this.data[i].male,
+  }));
+}
--- src/app/pages/dashboard/vertical-bar/vertical-bar.component.ts
+++ src/app/pages/dashboard/vertical-bar/vertical-bar.component.ts
@@ -1,12 +1,11 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { EChartOption } from 'echarts';
 
-const steps = [0, 18, 30, 42, 55, 67];
-const yAxisData = steps.map((step, i, arr) =>
-  i === arr.length - 1
-    ? `+${step}`
-    : `${step + (i === 0 ? 0 : 1)}-${steps[i + 1]}`,
-);
+export interface VerticalBarData {
+  label: string;
+  left: number;
+  right: number;
+}
 
 @Component({
   selector: 'ngx-vertical-bar',
@@ -17,88 +16,91 @@ const yAxisData = steps.map((step, i, arr) =>
 export class VerticalBarComponent {
   @Input() leftColor: string;
   @Input() rightColor: string;
-  @Input() data: Array<{ label: string; left: number; right: number }>;
+  private _data: VerticalBarData[] = [];
 
-  public chartOption: EChartOption<EChartOption.SeriesBar> = {
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow',
+  @Input() set data(data: VerticalBarData[]) {
+    this._data = data;
+    this.updateChartOption();
+  }
+  get data() {
+    return this._data;
+  }
+
+  public chartOption?: EChartOption<EChartOption.SeriesBar>;
+
+  public updateChartOption() {
+    this.chartOption = {
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        top: '3%',
+        containLabel: true,
       },
-    },
-    legend: {
-      data: ['נשים', 'גברים'],
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      top: '3%',
-      containLabel: true,
-    },
-    xAxis: [
-      {
-        type: 'value',
-        axisLine: {
-          show: false,
-        },
-        axisPointer: {
-          show: false,
-        },
-        axisLabel: {
-          formatter: Math.abs,
-        },
-        splitLine: {
-          lineStyle: {
-            opacity: 0.5,
+      xAxis: [
+        {
+          type: 'value',
+          axisLine: {
+            show: false,
+          },
+          axisPointer: {
+            show: false,
+          },
+          axisLabel: {
+            formatter: Math.abs,
+          },
+          splitLine: {
+            lineStyle: {
+              opacity: 0.5,
+            },
           },
         },
-      },
-    ],
-    yAxis: [
-      {
-        type: 'category',
-        data: yAxisData,
-        nameLocation: 'center',
-        splitLine: {
-          show: true,
-          lineStyle: {
-            // type: 'dashed',
-            opacity: 0.5,
+      ],
+      yAxis: [
+        {
+          type: 'category',
+          data: this.data.map(item => item.label),
+          nameLocation: 'center',
+          splitLine: {
+            show: true,
+            lineStyle: {
+              // type: 'dashed',
+              opacity: 0.5,
+            },
           },
         },
-      },
-    ],
-    series: [
-      {
-        name: 'male',
-        type: 'bar',
-        stack: 'main',
-        itemStyle: {
-          barBorderRadius: [0, 5, 5, 0],
-          color: '#48CAFF',
+      ],
+      series: [
+        {
+          name: 'right',
+          type: 'bar',
+          stack: 'main',
+          itemStyle: {
+            barBorderRadius: [0, 5, 5, 0],
+            color: '#48CAFF',
+          },
+          barWidth: 8,
+          label: {
+            show: false,
+          },
+          data: this.data.map(item => item.right),
         },
-        barWidth: 8,
-        label: {
-          show: false,
+        {
+          name: 'left',
+          type: 'bar',
+          stack: 'main',
+          itemStyle: {
+            barBorderRadius: [5, 0, 0, 5],
+            color: '#B6CA51',
+          },
+          barWidth: 8,
+          label: {
+            show: false,
+            position: 'left',
+          },
+          data: this.data.map(item => -item.left),
         },
-        data: [320, 302, 341, 374, 390, 450, 420],
-      },
-      {
-        name: 'female',
-        type: 'bar',
-        stack: 'main',
-        itemStyle: {
-          barBorderRadius: [5, 0, 0, 5],
-          color: '#B6CA51',
-        },
-        barWidth: 8,
-        label: {
-          show: false,
-          position: 'left',
-        },
-        data: [-120, -132, -101, -134, -190, -230, -210],
-      },
-    ],
-  };
+      ],
+    };
+  }
 }
