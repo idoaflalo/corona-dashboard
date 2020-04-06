ad8a70d8977d0ed456257dbb4476fed2b2d3e871
Guy Baron <guyb2000@gmail.com>
2020-04-06 15:28:11 +0300

    removed unused input

--- src/app/pages/dashboard/popover-trend-graph/popover-trend-graph.component.ts
+++ src/app/pages/dashboard/popover-trend-graph/popover-trend-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LinearGraphData } from '../ngx-linear-graph/interfaces';
 import { data } from '../epidemiologic-graph/mock';
 
